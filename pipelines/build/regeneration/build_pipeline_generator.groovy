ce9a0f51715dabd4423eff809720ebc09faf74de
sunyaqi <sunyaqi@loongson.cn>
2024-07-03 17:03:55 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator.groovy
+++ pipelines/build/regeneration/build_pipeline_generator.groovy
@@ -13,6 +13,7 @@ node('worker') {
         String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
         def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
         Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
+        println ADOPT_DEFAULTS_JSON 
         if (!ADOPT_DEFAULTS_JSON || !Map.isInstance(ADOPT_DEFAULTS_JSON)) {
             throw new Exception("[ERROR] No ADOPT_DEFAULTS_JSON found at ${ADOPT_DEFAULTS_FILE_URL} or it is not a valid JSON object. Please ensure this path is correct and leads to a JSON or Map object file. NOTE: Since this adopt's defaults and unlikely to change location, this is likely a network or GitHub issue.")
         }
