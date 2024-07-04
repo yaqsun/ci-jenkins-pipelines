01a45afd920397d965756dd7d54fd4f36ea5c9ab
sunyaqi <sunyaqi@loongson.cn>
2024-07-04 19:50:27 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator.groovy
+++ pipelines/build/regeneration/build_pipeline_generator.groovy
@@ -43,6 +43,8 @@ node('worker') {
     /*
     Changes dir to the user's repo. Use closures as functions aren't accepted inside node blocks
     */
+            def remoteConfigs = [ url: "https://github.com/adoptium/ci-jenkins-pipelines.git" ]
+            def repoBranch = "master"
         def checkoutUserPipelines = { ->
             checkout([$class: 'GitSCM',
                 branches: [ [ name: repoBranch ] ],
@@ -63,8 +65,6 @@ node('worker') {
             // Load credentials to be used in checking out. This is in case we are checking out a URL that is not Adopts and they don't have their ssh key on the machine.
             def checkoutCreds = (params.CHECKOUT_CREDENTIALS) ?: ''
             //remoteConfigs = [ url: repoUri ]
-            def remoteConfigs = [ url: "https://github.com/adoptium/ci-jenkins-pipelines.git" ]
-            def repoBranch = "master"
 /*
             if (checkoutCreds != '') {
                 // NOTE: This currently does not work with user credentials due to https://issues.jenkins.io/browse/JENKINS-60349
