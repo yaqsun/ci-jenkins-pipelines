bd24b60d0180c3d696fd0fdc9e1a9a53b0390ebb
sunyaqi <sunyaqi@loongson.cn>
2024-07-04 20:50:14 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -17,7 +17,12 @@ node('worker') {
          } catch (IOException e) {
         e.printStackTrace();
         }
-        Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
+         Map<String, ?> ADOPT_DEFAULTS_JSON = [:]
+        try {
+        ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
+         } catch (IOException e) {
+        e.printStackTrace();
+        }
         if (!ADOPT_DEFAULTS_JSON || !Map.isInstance(ADOPT_DEFAULTS_JSON)) {
             throw new Exception("[ERROR] No ADOPT_DEFAULTS_JSON found at ${ADOPT_DEFAULTS_FILE_URL} or it is not a valid JSON object. Please ensure this path is correct and leads to a JSON or Map object file. NOTE: Since this adopt's defaults and unlikely to change location, this is likely a network or GitHub issue.")
         }
