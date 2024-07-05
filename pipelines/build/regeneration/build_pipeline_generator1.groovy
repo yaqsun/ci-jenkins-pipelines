ce7f1c92b14c7d9187be8c58eabbc22ce562efe3
sunyaqi <sunyaqi@loongson.cn>
2024-07-05 14:46:48 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -9,8 +9,8 @@ file used as jenkinsfile to generator nightly and weekly pipeline
 node('worker') {
 //    try {
         // Pull in Adopt defaults
-        //String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
-        String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
+        String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
+        //String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
         def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
         //def getAdopt = null
         //URL getAdoptUrl = getAdoptUrl = new URL(ADOPT_DEFAULTS_FILE_URL)
