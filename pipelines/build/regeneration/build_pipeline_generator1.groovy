1f4fa213512ac605876109d5d0122ffff085f350
sunyaqi <sunyaqi@loongson.cn>
2024-07-05 11:02:08 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -24,7 +24,7 @@ node('worker') {
        def TEST_CONF = params.TEST_CONF ? params.TEST_CONF : ""
        println TEST_CONF
        //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
-       //def ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
+       def ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
 /*
          Map<String, ?> ADOPT_DEFAULTS_JSON = [:]
         try {
