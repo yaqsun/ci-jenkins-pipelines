1718ac34a0eeaf272a32914a8ca1a6950d23aec5
sunyaqi <sunyaqi@loongson.cn>
2024-07-05 11:43:27 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator.groovy
+++ pipelines/build/regeneration/build_pipeline_generator.groovy
@@ -10,11 +10,11 @@ node('worker') {
     try {
         // Pull in Adopt defaults
         String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
-        TEST_CONF = params.TEST_CONF ? params.TEST_CONF : ""
-        //def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
-        //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
-        Map ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
-        println ADOPT_DEFAULTS_JSON
+        //TEST_CONF = params.TEST_CONF ? params.TEST_CONF : ""
+        def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
+        Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
+        //Map ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
+        //println ADOPT_DEFAULTS_JSON
         if (!ADOPT_DEFAULTS_JSON || !Map.isInstance(ADOPT_DEFAULTS_JSON)) {
             throw new Exception("[ERROR] No ADOPT_DEFAULTS_JSON found at ${ADOPT_DEFAULTS_FILE_URL} or it is not a valid JSON object. Please ensure this path is correct and leads to a JSON or Map object file. NOTE: Since this adopt's defaults and unlikely to change location, this is likely a network or GitHub issue.")
         }
