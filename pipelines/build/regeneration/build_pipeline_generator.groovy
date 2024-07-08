04685b5b2b77f4ae699daa9f6eb20126bff02136
sunyaqi <sunyaqi@loongson.cn>
2024-07-08 15:15:44 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator.groovy
+++ pipelines/build/regeneration/build_pipeline_generator.groovy
@@ -79,10 +79,10 @@ node('worker') {
             //try {
             checkoutUserPipelines()
             println "checkoutUserPipelines() ======= 11111"
-            //def stash = checkout([$class: 'GitSCM',
-            //    branches: [ [ name: repoBranch ] ],
-            //    userRemoteConfigs: [ remoteConfigs ]
-            //])
+            def stash = checkout([$class: 'GitSCM',
+                branches: [ [ name: repoBranch ] ],
+                userRemoteConfigs: [ remoteConfigs ]
+            ])
             //println "Checked out commit: ${stash.GIT_COMMIT}"
             //} catch (IOException e) {
             //   e.printStackTrace();
