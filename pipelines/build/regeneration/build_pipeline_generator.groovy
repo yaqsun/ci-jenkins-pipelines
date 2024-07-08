bb760af2ad6a36c6a208b3509ac0ea5964d26a36
sunyaqi <sunyaqi@loongson.cn>
2024-07-08 10:53:53 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator.groovy
+++ pipelines/build/regeneration/build_pipeline_generator.groovy
@@ -53,10 +53,10 @@ node('worker') {
             def retiredVersions = [9, 10, 12, 13, 14, 15, 16, 18, 19, 20]
             def generatedPipelines = []
 
+            println "checkoutUserPipelines() ======= 11111"
             // Load git url and branch and gitBranch. These determine where we will be pulling user configs from.
             def repoUri = (params.REPOSITORY_URL) ?: DEFAULTS_JSON['repository']['pipeline_url']
             repoBranch = (params.REPOSITORY_BRANCH) ?: DEFAULTS_JSON['repository']['pipeline_branch']
-            println "checkoutUserPipelines() ======= 11111"
 
             // Load credentials to be used in checking out. This is in case we are checking out a URL that is not Adopts and they don't have their ssh key on the machine.
             def checkoutCreds = (params.CHECKOUT_CREDENTIALS) ?: ''
