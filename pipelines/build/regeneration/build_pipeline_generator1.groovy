0805c9f3b79fa2275330fa94edb4b097afd3024a
sunyaqi <sunyaqi@loongson.cn>
2024-07-09 14:57:05 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -10,8 +10,8 @@ node('worker') {
 //    try {
         // Pull in Adopt defaults
         //String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
-        //String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
-        //def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
+        String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
+        def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
          //getAdopt.setRequestMethod('GET')
          //getAdopt.connect()
         //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
