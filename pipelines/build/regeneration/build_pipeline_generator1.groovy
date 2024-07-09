7cc597841c6548ac5d1befa682a41ca67e7684c1
sunyaqi <sunyaqi@loongson.cn>
2024-07-09 14:46:25 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -10,11 +10,11 @@ node('worker') {
 //    try {
         // Pull in Adopt defaults
         //String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
-        String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
-        def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
+        //String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
+        //def getAdopt = new URL(ADOPT_DEFAULTS_FILE_URL).openConnection()
          //getAdopt.setRequestMethod('GET')
          //getAdopt.connect()
-        Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
+        //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
         //getAdopt.disconnect()
         //def getAdopt = null
         //URL getAdoptUrl  = new URL(ADOPT_DEFAULTS_FILE_URL)
@@ -38,7 +38,7 @@ node('worker') {
         e.printStackTrace();
         }
 */
-
+/*
         if (!ADOPT_DEFAULTS_JSON || !Map.isInstance(ADOPT_DEFAULTS_JSON)) {
             throw new Exception("[ERROR] No ADOPT_DEFAULTS_JSON found at ${ADOPT_DEFAULTS_FILE_URL} or it is not a valid JSON object. Please ensure this path is correct and leads to a JSON or Map object file. NOTE: Since this adopt's defaults and unlikely to change location, this is likely a network or GitHub issue.")
         }
@@ -50,10 +50,10 @@ node('worker') {
         if (!DEFAULTS_JSON || !Map.isInstance(DEFAULTS_JSON)) {
             throw new Exception("[ERROR] No DEFAULTS_JSON found at ${DEFAULTS_FILE_URL} or it is not a valid JSON object. Please ensure this path is correct and leads to a JSON or Map object file.")
         }
+*/
 
-
- //       Map remoteConfigs = [:]
- //       def repoBranch = null
+        Map remoteConfigs = [:]
+        def repoBranch = null
     /*
     Changes dir to Adopt's repo. Use closures as functions aren't accepted inside node blocks
     */
