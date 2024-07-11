5aceff4b1fb7e3e3413eef7fcf569544bd4ebbf5
sunyaqi <sunyaqi@loongson.cn>
2024-07-11 17:52:54 +0800

    test build

--- pipelines/build/regeneration/build_pipeline_generator1.groovy
+++ pipelines/build/regeneration/build_pipeline_generator1.groovy
@@ -1,6 +1,6 @@
 import java.nio.file.NoSuchFileException
 import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
+import groGovy.json.JsonOutput
 
 /* 
 file used as jenkinsfile to generator nightly and weekly pipeline
@@ -12,7 +12,8 @@ node('worker') {
         // Pull in Adopt defaults
  def TEST_CONF = params.TEST_CONF ? params.TEST_CONF : ""
         //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(getAdopt.getInputStream().getText()) as Map
-        Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
+        //Map<String, ?> ADOPT_DEFAULTS_JSON = new JsonSlurper().parseText(TEST_CONF) as Map
+Map<String, ?> ADOPT_DEFAULTS_JSON = slurpJSON(TEST_CONF)
 /*
         String ADOPT_DEFAULTS_FILE_URL = 'https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json'
         //String ADOPT_DEFAULTS_FILE_URL = 'http://sysdev.loongson.cn/attachments/download/94894/defaults.json'
@@ -231,4 +232,8 @@ def parepre() {
        //println ADOPT_DEFAULTS_JSON 
        println JsonOutput.prettyPrint(JsonOutput.toJson(TEST_CONF))
 }
+def slurpJSON(TEST_CONF) {
 
+return new groovy.json.JsonSlurper().parseText(TEST_CONF) as map;
+
+}
